31792024c668f1c2fa7aacbdb8b0f6dab0d7a3e3
Tomasz Pawłowski <t.pawlowski@live.com>
2017-09-17 18:00:16 +0200

    add function and detection of request GET/POST

--- nodeServer.js
+++ nodeServer.js
@@ -1,3 +1,17 @@
+//generowanie losowej zaokrąglonej
+function randomNumberFromRange(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
 var http = require('http');
-    http.createServer().listen(8888, '127.0.0.1');
+http.createServer(function (req, res) {
+    var sesionNumber = randomNumberFromRange(1, 10000);
+    console.log(sesionNumber);
+    if (req.method === "GET") {
+        console.log("request GET, number:", sesionNumber);
+    }
+    else if (req.method === "POST") {
+        console.log("request POST")
+    }
+}).listen(8888, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8888/');
